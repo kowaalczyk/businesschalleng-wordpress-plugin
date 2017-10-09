ccca61c52ae548ad55e1609c0fb450bc7edcda8a
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2017-10-09 16:46:57 +0200

    Add template selectors to functions.php

--- functions.php
+++ functions.php
@@ -12,5 +12,67 @@ function hsbc_theme_scripts() {
     wp_enqueue_script( 'hsbc-theme-materialize-script', get_template_directory_uri() . '/assets/js/materialize.min.js', array('hsbc-theme-jquery-script'), null, true );
     wp_enqueue_script( 'hsbc-theme-hsbc-script', get_template_directory_uri() . '/assets/js/hsbc.js', array('hsbc-theme-materialize-script'), null, true );
 }
-add_action( 'wp_enqueue_scripts', 'hsbc_theme_scripts' )
-?>
+add_action( 'wp_enqueue_scripts', 'hsbc_theme_scripts' );
+
+function hsbc_category_names($p) {
+    $categories = get_the_category($p->ID);
+    return array_column($categories, 'name');
+}
+
+function hsbc_is_post($p) {
+    $cat_names = hsbc_category_names($p);
+    return in_array('HSBC Posts', $cat_names)
+}
+
+function hsbc_is_component($c) {
+    $cat_names = hsbc_category_names($c);
+    return in_array('HSBC Components', $cat_names)
+}
+
+function hsbc_render_post($p) {
+    $cat_names = hsbc_category_names($p);
+
+    if(in_array('HSBC Standard Posts', $cat_names)):
+        return get_template_part('hsbc_post', 'standard');
+    endif;
+
+    if(in_array('HSBC List Posts', $cat_names)):
+        return get_template_part('hsbc_post', 'list');
+    endif;
+
+    if(in_array('HSBC External Media Posts', $cat_names)):
+        return get_template_part('hsbc_post', 'external_media');
+    endif;
+
+    if(in_array('HSBC Calendar Posts', $cat_names)):
+        return get_template_part('hsbc_post', 'calendar');
+    endif;
+
+    if(in_array('HSBC Team Posts', $cat_names)):
+        return get_template_part('hsbc_post', 'team');
+    endif;
+
+    if(in_array('HSBC Partner Posts', $cat_names)):
+        return get_template_part('hsbc_post', 'partner');
+    endif;
+}
+
+function hsbc_render_component($c) {
+    $cat_names = hsbc_category_names($c);
+
+    if(in_array('HSBC Button Components', $cat_names)):
+        return get_template_part('hsbc_component', 'button');
+    endif;
+
+    if(in_array('HSBC Text Components', $cat_names)):
+        return get_template_part('hsbc_component', 'text');
+    endif;
+
+    if(in_array('HSBC Team Member Components', $cat_names)):
+        return get_template_part('hsbc_component', 'team_member');
+    endif;
+
+    if(in_array('HSBC Team Components', $cat_names)):
+        return get_template_part('hsbc_component', 'team');
+    endif;
+}
